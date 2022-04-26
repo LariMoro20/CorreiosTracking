62f6aae260f95621e1e5739d01dee83aab35ecb8
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-04-26 12:47:57 -0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -64,11 +64,10 @@
           <q-avatar size="80px">
             <img src="/images/caminhao.png" />
           </q-avatar>
-          <div class="col-8 text-white">
-            <p>
-              Status: {{ evento.status }}<br />
+          <div class="col-8 text-white flex items-center">
+            <p class="q-pa-none q-ma-none">
+              {{ evento.status }}<br />
               Data: {{ evento.data }} {{ evento.hora }}<br />
-              Local:<br />
               {{ evento.local }}<br />
             </p>
           </div>
@@ -132,6 +131,6 @@ export default defineComponent({
   align-content: center;
   justify-content: center;
   align-items: center;
-  font-size: 24px;
+  font-size: 2.2rem;
 }
 </style>
