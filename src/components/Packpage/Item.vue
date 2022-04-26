4be107f07da22e1c6c3bc0112d99e86083a03a30
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-04-26 12:58:11 -0300

    [UPDATE] Item Component

---
+++ src/components/Packpage/Item.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex packpage__item q-pa-sm">
+    <q-avatar size="80px">
+      <img src="/images/caminhao.png" />
+    </q-avatar>
+    <div class="col-8 text-white flex items-center">
+      <p class="q-pa-none q-ma-none">
+        {{ packpage.status }}<br />
+        Data: {{ packpage.data }} {{ packpage.hora }}<br />
+        {{ packpage.local }}<br />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    packpage: {
+      type: [Array, Object],
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.packpage__item {
+  border-left: 3px dashed #bcbcbc;
+}
+.packpage__item::before {
+  content: "---";
+  color: #bcbcbc;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.2rem;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -60,18 +60,7 @@
         v-for="(evento, ikey) in thepackage.eventos"
         :key="ikey"
       >
-        <div class="flex packpage__item q-pa-sm">
-          <q-avatar size="80px">
-            <img src="/images/caminhao.png" />
-          </q-avatar>
-          <div class="col-8 text-white flex items-center">
-            <p class="q-pa-none q-ma-none">
-              {{ evento.status }}<br />
-              Data: {{ evento.data }} {{ evento.hora }}<br />
-              {{ evento.local }}<br />
-            </p>
-          </div>
-        </div>
+        <Item :packpage="evento" />
       </div>
     </div>
   </q-page>
@@ -81,9 +70,11 @@
 import { defineComponent, ref, onMounted, onBeforeMount } from "vue";
 import { api } from "boot/axios";
 import { useQuasar } from "quasar";
+import Item from "components/Packpage/Item.vue";
+
 export default defineComponent({
   name: "Correios",
-  components: {},
+  components: { Item },
   setup() {
     const tracking_code = ref("");
     const thepackage = ref([]);
@@ -120,17 +111,3 @@ export default defineComponent({
   },
 });
 </script>
-<style scoped>
-.packpage__item {
-  border-left: 3px dashed #bcbcbc;
-}
-.packpage__item::before {
-  content: "---";
-  color: #bcbcbc;
-  display: flex;
-  align-content: center;
-  justify-content: center;
-  align-items: center;
-  font-size: 2.2rem;
-}
-</style>
