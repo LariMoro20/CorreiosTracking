7727861cb44bad3ddf85c3f1f912ea5834cb6982
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-04-26 13:26:40 -0300

    Update Item.vue

--- src/components/Packpage/Item.vue
+++ src/components/Packpage/Item.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="flex packpage__item q-pa-sm">
-    <q-avatar size="80px">
-      <img src="/images/caminhao.png" />
-    </q-avatar>
-    <div class="col-8 text-white flex items-center">
-      <p class="q-pa-none q-ma-none">
-        {{ packpage.status }}<br />
-        Data: {{ packpage.data }} {{ packpage.hora }}<br />
-        {{ packpage.local }}<br />
-      </p>
+    <div class="flex">
+      <q-avatar size="80px">
+        <img src="/images/caminhao.png" />
+      </q-avatar>
+      <div class="text-white flex items-center">
+        <p class="q-pa-none q-ma-none">
+          {{ packpage.status }}<br />
+          Data: {{ packpage.data }} {{ packpage.hora }}<br />
+          <span v-if="status.length" style="font-size: 12px"
+            >{{ status[0] }} <br />Para:{{ status[1] }}</span
+          >
+          <span v-else> {{ packpage.local }}</span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +25,15 @@ export default {
       type: [Array, Object],
       default: null,
     },
+  },
+  data: () => ({
+    status: [],
+  }),
+
+  mounted() {
+    if (this.packpage.subStatus[0]) {
+      this.status = this.packpage.subStatus[0].split("Para:");
+    }
   },
 };
 </script>
